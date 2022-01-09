a5804907f3f2e283f37bd030deb89d661233ee54
Rafeh Qazi <qazi@cleverprogrammer.com>
2022-01-09 14:59:56 -0800

    Added Qazi.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h2>Hi i am Isaac 🚀</h2>
 
       <h2>Hi i am Sirkickassalot</h2>
       <h2>this is a change</h2>
+      <h2>This is Qazi</h2>
 
       <h2>Hi I'm Edith !</h2>
       <h1
         style={{
-          backgroundColor: "yellow",
-          height: "200px",
-          width: "200px",
-          border: "15px dotted green",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "blue",
+          backgroundColor: 'yellow',
+          height: '200px',
+          width: '200px',
+          border: '15px dotted green',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'blue',
         }}
       >
         HIIIII lets get it team!! This is aly
       </h1>
-
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
