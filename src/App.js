5f5421abcaa52798ca30fa6b666417704357b736
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-01-09 11:44:03 -0800

    adds aly h1 tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h2>Hi i am Isaac 🚀</h2>
+      <h1>HIIIII lets get it team!! This is aly</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
